927b944b544b24167549756656fa10301cc74fd1
Yonghyu Ban <yonghyu@empo.im>
2021-07-14 13:17:32 +0900

    Add setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mareel Cisco config manipulation daemon
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='ciscocfg',
+     version='0.1',
+     scripts=['__init__.py'],
+     author="Mareel Team",
+     author_email="admin@mareel.io",
+     description="Cisco configuration manipulation daemon",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/mareel-io/ciscocfg",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GPL 3.0",
+         "Operating System :: OS Independent",
+     ],
+ )
